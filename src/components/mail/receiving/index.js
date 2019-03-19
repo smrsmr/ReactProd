8560a298df90a476850f7343c5f37b5e8a1f41d8
smr <smr@qq.com>
2019-03-19 18:22:25 +0800

    添加mock api 接口数据

--- src/components/mail/receiving/index.js
+++ src/components/mail/receiving/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-
+import api from '@/api/axios';
+import '@/mock';
 export class Receiving extends Component {
+  componentDidMount() {
+    api.mockdata('/data/index')
+      .then(res => {
+        console.log(res);
+      });
+  }
   render() {
     return (
       <div>
